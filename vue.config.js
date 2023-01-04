f8b0c30394b8707c8a9f16895fadae2aa3069e88
tsCalm <dlatkdals93@gmail.com>
2023-01-04 19:35:45 +0900

    feat: vuetify

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "./dist",
   publicPath: "/vue-upbit/",
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 });
