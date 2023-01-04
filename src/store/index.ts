7021245fc4e1a60b3f2eb30daf2f58a14e20bade
tsCalm <dlatkdals93@gmail.com>
2023-01-04 18:14:20 +0900

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    counter: 0,
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
