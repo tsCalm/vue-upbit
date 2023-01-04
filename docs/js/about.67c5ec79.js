afb3cfa4cc51208fb5f7493c57bb4eae82cf52e1
tsCalm <dlatkdals93@gmail.com>
2023-01-04 19:36:06 +0900

    Update

---
+++ docs/js/about.67c5ec79.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_upbit"]=self["webpackChunkvue_upbit"]||[]).push([[443],{8307:function(u,n,t){t.r(n),t.d(n,{default:function(){return f}});var e=t(3396);const a={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function i(u,n){return(0,e.wg)(),(0,e.iD)("div",a,s)}var c=t(89);const o={},b=(0,c.Z)(o,[["render",i]]);var f=b}}]);
+//# sourceMappingURL=about.67c5ec79.js.map
